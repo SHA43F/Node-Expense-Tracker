d6d65916e020d9237521ec75c4508b4173509149
SHA43F <shareefpmk44@gmail.com>
2023-01-10 17:53:01 +0530

    2.6) Password Encryption

--- controllers/signInCtrl.js
+++ controllers/signInCtrl.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const bcrypt = require("bcrypt");
 const Users = require("../modals/users");
 
 const rootDir = require("../util/rootDir");
@@ -16,11 +17,20 @@ exports.postSignInData = (req, res, next) => {
   Users.findAll().then((users) => {
     const emailExistance = users.find((user) => user.email === req.body.email);
     if (emailExistance) {
-      if (emailExistance.password === req.body.password) {
-        res.send("<h3>Logged In successfully</h3>");
-      } else {
-        res.status(401).send("<h3>Incorrect password.. Try again</h3>");
-      }
+      bcrypt.compare(
+        req.body.password,
+        emailExistance.password,
+        (err, result) => {
+          if (err) {
+            res.status(500).send("<h3>Some Error Happened..</h3>");
+          }
+          if (result) {
+            res.send("<h3>Logged In successfully</h3>");
+          } else {
+            res.status(401).send("<h3>Incorrect password.. Try again</h3>");
+          }
+        }
+      );
     } else {
       res.status(404).send("<h3>No Email Found</h3>");
     }
--- controllers/signUpCtrl.js
+++ controllers/signUpCtrl.js
@@ -1,23 +1,30 @@
 const path = require("path");
 const rootDir = require("../util/rootDir");
 const Users = require("../modals/users");
+const bcrypt = require("bcrypt");
 
 exports.getSignUpData = (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "signUp.html"));
 };
 
-exports.postSignUpData = (req, res, next) => {
-  Users.create({
-    userName: req.body.userName,
-    email: req.body.email,
-    password: req.body.password
-  })
-    .then(() => {
-      res.redirect("/signUp");
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send(`<p>${err}</p>`);
-      // res.redirect("/");
+exports.postSignUpData = async (req, res, next) => {
+  try {
+    const { userName, email, password } = req.body;
+    bcrypt.hash(password, 10, async (err, hash) => {
+      await Users.create({
+        userName: userName,
+        email: email,
+        password: hash
+      })
+        .then(() => {
+          res.redirect("/signUp");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.send(`<p>${err}</p>`);
+        });
     });
+  } catch (err) {
+    console.log(err);
+  }
 };
