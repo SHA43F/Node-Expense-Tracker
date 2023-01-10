f0c3e73d619624e793188d3feb206d8d17518a02
SHA43F <shareefpmk44@gmail.com>
2023-01-10 21:52:24 +0530

    2.7)Expenses Feature

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const sequelize = require("./database/sqlDatabase");
 
 const signUpRouter = require("./routes/signUpRouter");
 const signInRouter = require("./routes/signInRouter");
+const expenseRouter = require("./routes/expenseRouter");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(signUpRouter);
 app.use(signInRouter);
+app.use(expenseRouter);
 
 sequelize
   .sync()
