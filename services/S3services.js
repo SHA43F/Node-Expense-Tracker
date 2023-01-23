06efb5d4e2a498d8421bfe501717057823050800
SHA43F <113891141+SHA43F@users.noreply.github.com>
2023-01-23 15:52:00 +0530

    Update S3services.js

--- services/S3services.js
+++ services/S3services.js
@@ -1,9 +1,6 @@
 const AWS = require("aws-sdk");
 
 exports.uploadToS3 = (data, filename) => {
-  // const BUCKET_NAME = "expensetracker98";
-  // const IAM_USER_KEY = "AKIAUAEFAKMYEYYY52H4";
-  // const ACCESS_SECRET_KEY = "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";
 
   const s3Bucket = new AWS.S3({
     accessKeyId: process.env.IAM_USER_KEY,
